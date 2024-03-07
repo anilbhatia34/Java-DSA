e4d164a8118037abe5cf3417862e925b769ead94
Anil <anilbhatia342gmail.com>
2024-03-07 12:32:02 +0530

    changes

--- src/TakingInput.java
+++ src/TakingInput.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 public class TakingInput {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+
+        // for taking string input we will use  sc.next() only ,
+        // for taking whole sentence as input then we will use nextLine();
         System.out.println("Please Enter the Value");
         int val = sc.nextInt();
         System.out.println("You have Entered : " + val);
