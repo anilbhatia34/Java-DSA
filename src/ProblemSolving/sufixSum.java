d22b1f2e5f37a720704d108239f34c182ee31bcb
Anil <anilbhatia342gmail.com>
2024-03-22 19:36:48 +0530

    Suffix sum

--- src/ProblemSolving/sufixSum.java
+++ src/ProblemSolving/sufixSum.java
@@ -4,7 +4,7 @@ public class sufixSum {
     public static void main(String[] args){
         int[] A = { 1,2,3,4};
         int N = A.length;
-        //prefix sum = { 10,9,7,4 };
+        //sufix sum = { 10,9,7,4 };
         int[] sufiSum= new int[N];
         sufiSum[N-1] = A[N-1];
         for(int i= N-2; i>=0; i--){
